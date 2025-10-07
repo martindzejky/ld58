c835cb9c8bb8b6655e3f02a5f52ca4a1f44f78ae
Martin Jakubik <martindzejky@gmail.com>
2025-10-07 20:50:43 +0200

    fix the loud music bug

--- scripts/music.gd
+++ scripts/music.gd
@@ -1,20 +1,16 @@
 # music autoload
 extends Node
 
+@export var timer: Timer
 @export var notes_player: AudioStreamPlayer
-@export var note_timer_min: float = 1.2
-@export var note_timer_max: float = 2.0
-
-func _ready():
-  await note_timer()
-  play_note()
-
-func note_timer():
-  await get_tree().create_timer(randf_range(note_timer_min, note_timer_max)).timeout
+@export var note_timer_min: float = 4
+@export var note_timer_max: float = 8
 
 func play_note():
   if notes_player.playing: return
   notes_player.play()
-  await notes_player.finished
-  await note_timer()
-  call_deferred('play_note')
+  print('playing note', Time.get_ticks_msec())
+
+func _on_timer_timeout() -> void:
+  timer.wait_time = randf_range(note_timer_min, note_timer_max)
+  play_note()
